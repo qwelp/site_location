017f5f0ad2aee406199c7f434e142c03b6eae8a8
qwelp <qwelp@mail.ru>
2025-08-11 13:49:09 +0300

    feat: include module before using context

--- local/modules/qwelp.site_location/components/contacts.block/class.php
+++ local/modules/qwelp.site_location/components/contacts.block/class.php
@@ -1,10 +1,16 @@
 <?php
+
+use Bitrix\Main\Loader;
 use Qwelp\SiteLocation\Context;
 
 class QwelpSiteLocationContactsBlockComponent extends CBitrixComponent
 {
     public function executeComponent()
     {
+        if (!Loader::includeModule('qwelp.site_location')) {
+            return;
+        }
+
         if ($this->StartResultCache()) {
             $city = Context::getCurrent();
             $this->arResult['CITY'] = $city;
